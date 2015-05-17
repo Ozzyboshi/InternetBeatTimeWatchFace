c453cf86dd5b1d22e136fa1a6844dfc33b70f83d
Ozzyboshi <gun101@email.it>
2015-05-17 15:37:53 +0200

    New internet beat time watchface based on this tutorial : http://catinean.com/2015/03/07/creating-a-watch-face-with-android-wear-api/

---
+++ wear/src/main/java/com/ozzyboshi/internetbeattimewatchface/InternetBeatTimeWatchFace.java
@@ -0,0 +1,198 @@
+package com.ozzyboshi.internetbeattimewatchface;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.text.format.Time;
+import android.util.Log;
+import com.ozzyboshi.simpleandroidwatchface.R;
+
+import org.joda.time.DateTimeZone;
+
+import java.util.TimeZone;
+
+public class InternetBeatTimeWatchFace {
+
+    private static final String TIME_FORMAT_WITHOUT_SECONDS = "%02d.%02d";
+    private static final String TIME_FORMAT_WITH_SECONDS = TIME_FORMAT_WITHOUT_SECONDS + ".%02d";
+    private static final String DATE_FORMAT = "%02d.%02d.%d";
+    private static final int DATE_AND_TIME_DEFAULT_COLOUR = Color.WHITE;
+    private static final int BACKGROUND_DEFAULT_COLOUR = Color.BLACK;
+
+    private static final String TAG = "BEATS_SWATCH_FACE_DEBUG";
+
+    private final Paint timePaint;
+    private final Paint beatTimePaint;
+    private final Paint dateOnlyPaint;
+    private final Paint backgroundPaint;
+    private final Time time;
+    private org.joda.time.DateTime utc;
+    private final TimeZone beatZone;
+
+    public static boolean wakelockDebug=false;
+
+    private boolean shouldShowSeconds = true;
+    private int backgroundColour = BACKGROUND_DEFAULT_COLOUR;
+    private int dateAndTimeColour = DATE_AND_TIME_DEFAULT_COLOUR;
+
+    public static InternetBeatTimeWatchFace newInstance(Context context) {
+        Paint timePaint = new Paint();
+        timePaint.setColor(DATE_AND_TIME_DEFAULT_COLOUR);
+        timePaint.setTextSize(context.getResources().getDimension(R.dimen.time_size));
+        timePaint.setAntiAlias(true);
+
+        Paint beatTimePaint = new Paint();
+        beatTimePaint.setColor(DATE_AND_TIME_DEFAULT_COLOUR);
+        beatTimePaint.setTextSize(context.getResources().getDimension(R.dimen.date_size));
+        beatTimePaint.setTypeface(Typeface.create("cursive", Typeface.BOLD_ITALIC));
+        beatTimePaint.setAntiAlias(true);
+
+        Paint dateOnlyPaint = new Paint();
+        dateOnlyPaint.setColor(DATE_AND_TIME_DEFAULT_COLOUR);
+        dateOnlyPaint.setTextSize(context.getResources().getDimension(R.dimen.date_size));
+        dateOnlyPaint.setAntiAlias(true);
+
+        Paint backgroundPaint = new Paint();
+        backgroundPaint.setColor(BACKGROUND_DEFAULT_COLOUR);
+
+        return new InternetBeatTimeWatchFace(timePaint, beatTimePaint, dateOnlyPaint, backgroundPaint, new Time());
+    }
+
+    InternetBeatTimeWatchFace(Paint timePaint, Paint beatTimePaint, Paint dateOnlyPaint, Paint backgroundPaint, Time time) {
+        this.timePaint = timePaint;
+        this.beatTimePaint = beatTimePaint;
+        this.dateOnlyPaint = dateOnlyPaint;
+        this.backgroundPaint = backgroundPaint;
+        this.time = time;
+        utc = null;
+        beatZone = TimeZone.getTimeZone("GMT+1");
+    }
+
+    private double getBeats() {
+        //utc = new org.joda.time.DateTime(DateTimeZone.UTC);
+        //time.setToNow();
+        utc = new org.joda.time.DateTime(org.joda.time.DateTimeZone.forTimeZone(beatZone));
+        double swatch = (double)((utc.getHourOfDay() +0)%24)+(double) utc.getMinuteOfHour()/60 + (double) utc.getSecondOfMinute()/3600;
+        return new Double(swatch*1000/24).doubleValue();
+    }
+
+    public Double getSecondsToNextBeat() {
+        //time.setToNow();
+        double beats=getBeats();
+        String beatTimeText=String.format(java.util.Locale.ENGLISH, "%.3f", beats);
+
+        String milliBeats=beatTimeText.substring(beatTimeText.length() - 3);
+        Integer milliBeatsToNextBeat=1000-new Integer(milliBeats);
+        Log.d(TAG,"getSecondsToNextBeat - text : "+beatTimeText+" millibeats "+milliBeats+" millibeats to next beat "+milliBeatsToNextBeat.toString());
+
+        Double secondsToNextBeat = new Double(milliBeatsToNextBeat*0.0864);
+        Log.d(TAG,"getSecondsToNextBeat - next beat at "+secondsToNextBeat.doubleValue());
+        return secondsToNextBeat;
+    }
+
+    public void draw(Canvas canvas, Rect bounds) {
+
+        Log.d(TAG,"Start drawing watchface");
+        time.setToNow();
+        double beats=getBeats();
+        canvas.drawRect(0, 0, bounds.width(), bounds.height(), backgroundPaint);
+
+        String timeText = String.format(shouldShowSeconds ? TIME_FORMAT_WITH_SECONDS : TIME_FORMAT_WITHOUT_SECONDS, time.hour, time.minute, time.second);
+        float timeXOffset = computeXOffset(timeText, timePaint, bounds);
+        float timeYOffset = computeTimeYOffset(timeText, timePaint, bounds);
+        canvas.drawText(timeText, timeXOffset, timeYOffset, timePaint);
+
+
+
+        String beatTimeText=null;
+        if (shouldShowSeconds)
+            beatTimeText=String.format(java.util.Locale.ENGLISH,"@         %.2f",beats);
+        else
+            beatTimeText=String.format(java.util.Locale.ENGLISH,"@            %d",(int)beats);
+
+        float beatTimeXOffset = computeXOffset(beatTimeText, beatTimePaint, bounds);
+        float beatTimeYOffset = computeDateYOffset(beatTimeText, beatTimePaint);
+        canvas.drawText(beatTimeText, beatTimeXOffset, timeYOffset + beatTimeYOffset+10, beatTimePaint);
+
+        String beatDateText="@      "+utc.dayOfMonth().get()+"."+utc.monthOfYear().get()+"."+utc.year().get();
+        float beatDateXOffset = computeXOffset(beatDateText, beatTimePaint, bounds);
+
+        canvas.drawText(beatDateText, beatDateXOffset, timeYOffset + beatTimeYOffset+computeDateYOffset(beatTimeText, dateOnlyPaint)+10, beatTimePaint);
+
+        String dateOnlyText = String.format(DATE_FORMAT, time.monthDay, (time.month + 1), time.year);
+        float dateOnlyXOffset = computeXOffset(dateOnlyText, dateOnlyPaint, bounds);
+        //float dateOnlyYOffset = computeDateYOffset(dateOnlyText, dateOnlyPaint);
+        canvas.drawText(dateOnlyText, dateOnlyXOffset, timeYOffset / 2, dateOnlyPaint);
+        Log.d(TAG,"End drawing watchface");
+    }
+
+    private float computeXOffset(String text, Paint paint, Rect watchBounds) {
+        float centerX = watchBounds.exactCenterX();
+        float timeLength = paint.measureText(text);
+        return centerX - (timeLength / 2.0f);
+    }
+
+    private float computeTimeYOffset(String timeText, Paint timePaint, Rect watchBounds) {
+        float centerY = watchBounds.exactCenterY();
+        Rect textBounds = new Rect();
+        timePaint.getTextBounds(timeText, 0, timeText.length(), textBounds);
+        int textHeight = textBounds.height();
+        return centerY + (textHeight / 2.0f);
+    }
+
+    private float computeDateYOffset(String dateText, Paint datePaint) {
+        Rect textBounds = new Rect();
+        datePaint.getTextBounds(dateText, 0, dateText.length(), textBounds);
+        return textBounds.height() + 10.0f;
+    }
+
+    public void setAntiAlias(boolean antiAlias) {
+        timePaint.setAntiAlias(antiAlias);
+        beatTimePaint.setAntiAlias(antiAlias);
+        dateOnlyPaint.setAntiAlias(antiAlias);
+    }
+
+    public void updateDateAndTimeColourTo(int colour) {
+        dateAndTimeColour = colour;
+        timePaint.setColor(colour);
+        beatTimePaint.setColor(colour);
+        dateOnlyPaint.setColor(colour);
+    }
+
+    public void updateTimeZoneWith(String timeZone) {
+        time.clear(timeZone);
+        time.setToNow();
+    }
+
+    public void setShowSeconds(boolean showSeconds) {
+        shouldShowSeconds = showSeconds;
+    }
+
+    public void updateBackgroundColourTo(int colour) {
+        backgroundColour = colour;
+        backgroundPaint.setColor(colour);
+    }
+
+    public void restoreBackgroundColour() {
+        backgroundPaint.setColor(backgroundColour);
+    }
+
+    public void updateBackgroundColourToDefault() {
+        backgroundPaint.setColor(BACKGROUND_DEFAULT_COLOUR);
+    }
+
+    public void updateDateAndTimeColourToDefault() {
+        timePaint.setColor(DATE_AND_TIME_DEFAULT_COLOUR);
+        beatTimePaint.setColor(DATE_AND_TIME_DEFAULT_COLOUR);
+        dateOnlyPaint.setColor(DATE_AND_TIME_DEFAULT_COLOUR);
+    }
+
+    public void restoreDateAndTimeColour() {
+        timePaint.setColor(dateAndTimeColour);
+        beatTimePaint.setColor(dateAndTimeColour);
+        dateOnlyPaint.setColor(dateAndTimeColour);
+    }
+}
---
+++ wear/src/main/java/com/ozzyboshi/internetbeattimewatchface/InternetBeatTimeWatchFaceService.java
@@ -0,0 +1,303 @@
+package com.ozzyboshi.internetbeattimewatchface;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.PowerManager;
+import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.support.wearable.watchface.WatchFaceStyle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataItemBuffer;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.Wearable;
+
+import java.util.concurrent.TimeUnit;
+
+public class InternetBeatTimeWatchFaceService extends CanvasWatchFaceService {
+
+    private static final long TICK_PERIOD_MILLIS = TimeUnit.SECONDS.toMillis(1);
+    private PowerManager.WakeLock wakeLock;
+    private static final String TAG = "BEATS_SWATCH_FACE_DEBUG";
+
+    @Override
+    public Engine onCreateEngine() {
+        return new SimpleEngine();
+    }
+
+    private class SimpleEngine extends CanvasWatchFaceService.Engine implements
+            GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+        private static final String ACTION_TIME_ZONE = "time-zone";
+
+        private InternetBeatTimeWatchFace watchFace;
+        private Handler timeTick;
+        private GoogleApiClient googleApiClient;
+
+        @Override
+        public void onCreate(SurfaceHolder holder) {
+            super.onCreate(holder);
+
+            setWatchFaceStyle(new WatchFaceStyle.Builder(InternetBeatTimeWatchFaceService.this)
+                    .setCardPeekMode(WatchFaceStyle.PEEK_MODE_SHORT)
+                    .setAmbientPeekMode(WatchFaceStyle.AMBIENT_PEEK_MODE_HIDDEN)
+                    .setBackgroundVisibility(WatchFaceStyle.BACKGROUND_VISIBILITY_INTERRUPTIVE)
+                    .setShowSystemUiTime(false)
+                    .build());
+
+            timeTick = new Handler(Looper.myLooper());
+            startTimerIfNecessary();
+
+            watchFace = InternetBeatTimeWatchFace.newInstance(InternetBeatTimeWatchFaceService.this);
+            googleApiClient = new GoogleApiClient.Builder(InternetBeatTimeWatchFaceService.this)
+                    .addApi(Wearable.API)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+        }
+
+        private void startTimerIfNecessary() {
+            Log.d(TAG,"Start of startTimerIfNecessary - remove callbacks");
+            timeTick.removeCallbacks(timeRunnable);
+            if (isVisible() && !isInAmbientMode()) {
+                timeTick.post(timeRunnable);
+            }
+            else if (isVisible() && isInAmbientMode() && watchFace!=null ) {
+                Log.d(TAG,"End of startTimerIfNecessary - start timeTick with beatsRunnable");
+                timeTick.post(beatsRunnable);
+            }
+            PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+            wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                    "WatchFaceWakelockTag");
+            wakeLock.acquire();
+            watchFace.wakelockDebug=wakeLock.isHeld();
+
+        }
+
+        private final Runnable timeRunnable = new Runnable() {
+            @Override
+            public void run() {
+                onSecondTick();
+
+                if (isVisible() && !isInAmbientMode()) {
+                    timeTick.postDelayed(this, TICK_PERIOD_MILLIS);
+                }
+            }
+        };
+
+        private final Runnable beatsRunnable = new Runnable() {
+            @Override
+            public void run() {
+
+                if (isVisible() && isInAmbientMode()) {
+                    Double secondsToNextBeat=watchFace.getSecondsToNextBeat();
+
+                    // If secondstonextbeat<60 I dont need to post a runnable because onTimeTick comes first
+                    // I also release the wakelock to preserve battery
+                    /*if (secondsToNextBeat>60) {
+                        if (wakeLock.isHeld()) {
+                            Log.d(TAG, "wakelock released");
+                            wakeLock.release();
+                            watchFace.wakelockDebug=wakeLock.isHeld();
+                        }
+
+                        return ;
+                    }*/
+
+                    Double milliSecondsToNextBeat=secondsToNextBeat*1000;
+                    Log.d(TAG,"beatsRunnable - schedule next draw at "+milliSecondsToNextBeat.longValue());
+                    timeTick.postDelayed(this,milliSecondsToNextBeat.longValue());
+                }
+                Log.d(TAG,"beatsRunnable - invalidate and draw");
+                onTimeTick();
+            }
+        };
+
+        private void onSecondTick() {
+            invalidateIfNecessary();
+        }
+
+        private void invalidateIfNecessary() {
+            if (isVisible() && !isInAmbientMode()) {
+                invalidate();
+            }
+        }
+
+        @Override
+        public void onVisibilityChanged(boolean visible) {
+            super.onVisibilityChanged(visible);
+            if (visible) {
+                registerTimeZoneReceiver();
+                googleApiClient.connect();
+                Log.d(TAG,"Face visible, acquire wakelock");
+                if (!wakeLock.isHeld()) wakeLock.acquire();
+            } else {
+                Log.d(TAG,"Face no more visible, release wakelock");
+                if (wakeLock.isHeld()) wakeLock.release();
+                unregisterTimeZoneReceiver();
+                releaseGoogleApiClient();
+            }
+
+            startTimerIfNecessary();
+        }
+
+        private void releaseGoogleApiClient() {
+            if (googleApiClient != null && googleApiClient.isConnected()) {
+                Wearable.DataApi.removeListener(googleApiClient, onDataChangedListener);
+                googleApiClient.disconnect();
+            }
+        }
+
+        private void unregisterTimeZoneReceiver() {
+            unregisterReceiver(timeZoneChangedReceiver);
+        }
+
+        private void registerTimeZoneReceiver() {
+            IntentFilter timeZoneFilter = new IntentFilter(Intent.ACTION_TIMEZONE_CHANGED);
+            registerReceiver(timeZoneChangedReceiver, timeZoneFilter);
+        }
+
+        private BroadcastReceiver timeZoneChangedReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (Intent.ACTION_TIMEZONE_CHANGED.equals(intent.getAction())) {
+                    watchFace.updateTimeZoneWith(intent.getStringExtra(ACTION_TIME_ZONE));
+                }
+            }
+        };
+
+        @Override
+        public void onDraw(Canvas canvas, Rect bounds) {
+            super.onDraw(canvas, bounds);
+            watchFace.draw(canvas, bounds);
+        }
+
+        @Override
+        public void onTimeTick() {
+
+
+            /*Double secondsToNextBeat=watchFace.getSecondsToNextBeat();
+            if (secondsToNextBeat<60) {
+                timeTick.removeCallbacks(this.beatsRunnable);
+
+                if (!wakeLock.isHeld()) {
+                    Log.d(TAG,"acquired wakelock");
+                    wakeLock.acquire();
+                    watchFace.wakelockDebug=wakeLock.isHeld();
+                }
+                Double milliSecondsToNextBeat = secondsToNextBeat * 1000;
+                Log.d(TAG, "beatsRunnable after onTimetick invalidate - schedule next draw at " + milliSecondsToNextBeat.longValue());
+
+                timeTick.postDelayed(this.beatsRunnable, milliSecondsToNextBeat.longValue());
+            }*/
+            super.onTimeTick();
+            invalidate();
+        }
+
+        @Override
+        public void onAmbientModeChanged(boolean inAmbientMode) {
+            super.onAmbientModeChanged(inAmbientMode);
+            watchFace.setAntiAlias(!inAmbientMode);
+            watchFace.setShowSeconds(!isInAmbientMode());
+
+            if (inAmbientMode) {
+                watchFace.updateBackgroundColourToDefault();
+                watchFace.updateDateAndTimeColourToDefault();
+            } else {
+                watchFace.restoreBackgroundColour();
+                watchFace.restoreDateAndTimeColour();
+            }
+
+            invalidate();
+
+            startTimerIfNecessary();
+        }
+
+        @Override
+        public void onConnected(Bundle bundle) {
+            Log.d(TAG, "connected GoogleAPI");
+
+            Wearable.DataApi.addListener(googleApiClient, onDataChangedListener);
+            Wearable.DataApi.getDataItems(googleApiClient).setResultCallback(onConnectedResultCallback);
+        }
+
+        private final DataApi.DataListener onDataChangedListener = new DataApi.DataListener() {
+            @Override
+            public void onDataChanged(DataEventBuffer dataEvents) {
+                for (DataEvent event : dataEvents) {
+                    if (event.getType() == DataEvent.TYPE_CHANGED) {
+                        DataItem item = event.getDataItem();
+                        processConfigurationFor(item);
+                    }
+                }
+
+                dataEvents.release();
+                invalidateIfNecessary();
+            }
+        };
+
+        private void processConfigurationFor(DataItem item) {
+            if (WatchfaceSyncCommons.PATH.equals(item.getUri().getPath())) {
+                DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                if (dataMap.containsKey(WatchfaceSyncCommons.KEY_BACKGROUND_COLOUR)) {
+                    String backgroundColour = dataMap.getString(WatchfaceSyncCommons.KEY_BACKGROUND_COLOUR);
+                    watchFace.updateBackgroundColourTo(Color.parseColor(backgroundColour));
+                }
+
+                if (dataMap.containsKey(WatchfaceSyncCommons.KEY_DATE_TIME_COLOUR)) {
+                    String timeColour = dataMap.getString(WatchfaceSyncCommons.KEY_DATE_TIME_COLOUR);
+                    watchFace.updateDateAndTimeColourTo(Color.parseColor(timeColour));
+                }
+            }
+        }
+
+        private final ResultCallback<DataItemBuffer> onConnectedResultCallback = new ResultCallback<DataItemBuffer>() {
+            @Override
+            public void onResult(DataItemBuffer dataItems) {
+                for (DataItem item : dataItems) {
+                    processConfigurationFor(item);
+                }
+
+                dataItems.release();
+                invalidateIfNecessary();
+            }
+        };
+
+        @Override
+        public void onConnectionSuspended(int i) {
+            Log.e(TAG, "suspended GoogleAPI");
+        }
+
+        @Override
+        public void onConnectionFailed(ConnectionResult connectionResult) {
+            Log.e(TAG, "connectionFailed GoogleAPI");
+        }
+
+        @Override
+        public void onDestroy() {
+            Log.d(TAG,"Destroy engine");
+            wakeLock.release();
+            watchFace.wakelockDebug=wakeLock.isHeld();
+            timeTick.removeCallbacks(timeRunnable);
+            releaseGoogleApiClient();
+
+            super.onDestroy();
+        }
+    }
+}
